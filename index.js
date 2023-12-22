f1e96f2a6d6d8b29fbbb1e6859d2f72ede5c9cd2
keni2002 <yerbisuniverses@gmail.com>
2023-12-22 17:02:49 -0500

    add: nuevo boton para jugar de nuevo

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ let arr = [
 	[0, 0, 0],
 	[0, 0, 0]
 ]
-let players = ["","",0,0]
-let yoGane = [true,0];
+let players = ["", "", 0, 0]
+let yoGane = [true, 0];
 let baldo = document.getElementsByClassName('baldo')
 let tiles = document.getElementsByClassName('tile')
 let message = document.getElementById('message')
@@ -16,31 +16,31 @@ let player2 = document.getElementById('player2');
 let form = document.getElementById('form')
 let turnos = document.querySelector('p');
 
-start.addEventListener('click', (e)=>{
+start.addEventListener('click', (e) => {
 	e.preventDefault();
-	console.log(player1.value,player2.value)
-	if(player1.value !== '' || player2.value !==''){
-	players[0] = player1.value
-	players[1] = player2.value
-	player1.value =""
-	player2.value = ""
-	form.style = 'display: none'
-	turnos.style = 'display: block'
-	turnos.innerHTML = `${players[0]}, es tu turno`
-	yoGane[0] = false
-	} else{
+	console.log(player1.value, player2.value)
+	if (player1.value !== '' || player2.value !== '') {
+		players[0] = player1.value
+		players[1] = player2.value
+		player1.value = ""
+		player2.value = ""
+		form.style = 'display: none'
+		turnos.style = 'display: block'
+		turnos.innerHTML = `${players[0]}, es tu turno`
+		yoGane[0] = false
+	} else {
 		alert("ponga nombres")
 	}
 })
 function escribir(x, y) {
-	if (baldo[x].children[y].innerHTML == "" && yoGane[0]==false) {
+	if (baldo[x].children[y].innerHTML == "" && yoGane[0] == false) {
 		if (turn == 1) {
 			baldo[x].children[y].innerHTML = "X";
 			arr[x][y] = 1
 			yoGane[1]++;
 			turnos.innerHTML = `${players[1]}, es tu turno`
 			turn = 2
-			
+
 			test()
 			return
 		} else { turn == 2 } {
@@ -48,30 +48,33 @@ function escribir(x, y) {
 			turnos.innerHTML = `${players[0]}, es tu turno`
 			arr[x][y] = 2
 			yoGane[1]++;
-			
+
 			turn = 1
 			test()
-			return 
+			return
 		}
 	}
 }
 function test() {
-	
-	let val = finished(arr,3);
-	
-	if(yoGane[1]==9){
-		for(let i in tiles){
-			tiles[i].style='background-color: #ae0505';
-			
+
+	let val = finished(arr, 3);
+
+	if (yoGane[1] == 9) {
+		for (let i in tiles) {
+			tiles[i].style = 'background-color: #ae0505';
+			turnos.innerHTML = ``
 		}
 	}
-	if(val!== -1) {
-		message.innerHTML=`Felicidades, ${val == 1 ? players[0] : players[1]}, has Ganado`
-		yoGane[0]=true
-		for(let i in tiles){
-			tiles[i].style='background-color: #008000';
-			
+	if (val !== -1) {
+		message.innerHTML = `Felicidades, ${val == 1 ? players[0] : players[1]}, has Ganado`
+		turnos.innerHTML = ``
+		yoGane[0] = true
+		for (let i in tiles) {
+			tiles[i].style = 'background-color: #008000';
+
 		}
+		if (val == 1) players[2]++
+		if (val == 2) players[3]++
 	}
 }
 
@@ -85,7 +88,7 @@ function finished(arr, size) {
 	for (i = 0; i < size; i++) {
 		for (j = 0; j < size - 1; j++) {
 			equal = arr[j][i] == arr[j + 1][i]
-			if(!equal) break;
+			if (!equal) break;
 		}
 		if (equal && arr[j][i] !== 0) return arr[j][i]
 		equal = true
@@ -94,32 +97,39 @@ function finished(arr, size) {
 	for (i = 0; i < size; i++) {
 		for (j = 0; j < size - 1; j++) {
 			equal = arr[i][j] == arr[i][j + 1]
-			if(!equal) break;
+			if (!equal) break;
 		}
 		if (equal && arr[i][j] !== 0) return arr[i][j]
 		equal = true
 	}
 	//diagonal
-	if (arr[0][0] == arr[1][1] && arr[1][1] == arr[2][2] && arr[2][2]!=0) return arr[2][2]
-	if (arr[0][2] == arr[1][1] && arr[1][1] == arr[2][0] && arr[2][0]!=0) return arr[2][0]
+	if (arr[0][0] == arr[1][1] && arr[1][1] == arr[2][2] && arr[2][2] != 0) return arr[2][2]
+	if (arr[0][2] == arr[1][1] && arr[1][1] == arr[2][0] && arr[2][0] != 0) return arr[2][0]
 
 	return -1;
 }
 
-
-function resetear() {
-	for(let i in tiles){
-		tiles[i].innerHTML=""
-		yoGane = [false,0];
+function empty() {
+	for (let i in tiles) {
+		tiles[i].innerHTML = ""
+		yoGane = [false, 0];
 		turn = 1
 		arr = [
 			[0, 0, 0],
 			[0, 0, 0],
 			[0, 0, 0]
 		]
-		tiles[i].style='background-color:  #014067';
-		message.innerHTML=""
-		form.style = 'display: block'
+		tiles[i].style = 'background-color:  #014067';
 	}
+	message.innerHTML = ""
+
 }
+function resetear() {
+	empty();
 	
+	form.style = 'display: block'
+	yoGane[0] = true
+}
+function denuevo() {
+	empty()
+}
